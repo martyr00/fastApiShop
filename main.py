c4677e4887e6586c05f6a350e114e42485b4e27c
martyr00 <leonyvb04@gmail.com>
2024-06-09 18:26:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Online store'
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
